ce1c5dcb86703735c977a35f7388eed42523c592
Africa <ndouken@gmail.com>
2021-02-10 03:28:21 +0100

    Adding GSAP

---
+++ app.js
@@ -0,0 +1,25 @@
+let controller;
+let slideScene;
+
+function animateSlides() {
+  //Init controller
+  controller = new ScrollMagic.Controller();
+  //select some things
+  const slider = document.querySelectorAll(".slide");
+  const nav = document.querySelectorAll(".nav-header");
+  //loop over each slide
+  slider.forEach((slide) => {
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
+    //GSAP
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+    slideTl.fromTo(revealImg, { X: "0%" }, { x: "100%" });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+    slideTl.fromTo(revealText, { X: "0%" }, { x: "100%" }, "-=0.75");
+    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+  });
+}
+animateSlides();
