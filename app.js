cd7c824cac1cae4185da06a859473879923bc6d1
Africa <ndouken@gmail.com>
2021-02-12 04:08:22 +0100

    Scroll Animation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
   //Init controller
@@ -8,7 +9,7 @@ function animateSlides() {
   const slider = document.querySelectorAll(".slide");
   const nav = document.querySelectorAll(".nav-header");
   //loop over each slide
-  slider.forEach((slide) => {
+  slider.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
@@ -20,6 +21,40 @@ function animateSlides() {
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideTl.fromTo(revealText, { X: "0%" }, { x: "100%" }, "-=0.75");
     slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+    //Create scene
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      trigerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "slide",
+      })
+      .addTo(controller);
+    //New Animation
+    const pageTl = gsap.timeline();
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    pageTl.fromTo(nextSlide, { y: "0%" }, { y: "70%" });
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageTl.fromTo(nextSlide, { y: "70%" }, { y: "0%" }, "-=0.5");
+    //Create a new scene
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "page",
+        indent: 200,
+      })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTl)
+      .addTo(controller);
   });
 }
 animateSlides();
